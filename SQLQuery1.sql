df6364fa35678bec97686234124e20fc81218917
Sanjeev Kumar Das <sanjeev.das@marlabs.com>
2021-04-20 01:47:49 +0530

    test2

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,7 +1,10 @@
-USE Development;  
-
 CREATE TABLE S3Data1 (
 id [int] IDENTITY(1,1) NOT NULL,
 file_id nvarchar(500) NOT NULL,
 name nvarchar(500) NOT NULL
+);
+
+CREATE TABLE dbo.S3 (
+id [int] IDENTITY(1,1) NOT NULL,
+is_linc nvarchar(50) NOT NULL
 );
